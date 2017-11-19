c7efc3399420cd4c38cd815b7d8fa0830c4ee9dc
LaToya Green <latoyargreen@hotmail.com>
2017-11-19 07:15:09 -0600

    Initial Commit

---
+++ Split Sheetz/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Split Sheetz
+//
+//  Created by Verve on 11/19/17.
+//  Copyright © 2017 Verve. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
